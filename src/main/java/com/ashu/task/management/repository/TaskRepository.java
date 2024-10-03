cf6e05ac6b6dc9805119d5ff856d73e383fd6843
Ashutosh Sharma <125564039+ashu12355@users.noreply.github.com>
2024-10-03 19:03:38 +0530

    Update TaskRepository.java
    Description Added

--- src/main/java/com/ashu/task/management/repository/TaskRepository.java
+++ src/main/java/com/ashu/task/management/repository/TaskRepository.java
@@ -7,3 +7,5 @@ import com.ashu.task.management.model.Task;
 public interface TaskRepository extends JpaRepository<Task,Long> {
     
 }
+
+//JpaRepository<Task, Long>: This is a Spring Data JPA interface that provides CRUD (Create, Read, Update, Delete) operations and other database-related methods for the Task entity. The Task represents the type of entity, and Long is the type of the primary key (ID) of that entity.
